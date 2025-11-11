ec47f4dac118627ab59a7ce6067954420a4796ca
“IhebKtr” <real.iheb2@gmail.com>
2025-11-11 10:55:45 +0100

    Implement department management application (Prosit 10)

---
+++ src/tn/esprit/gestionemployes/DepartementHashSet.java
@@ -0,0 +1,51 @@
+package tn.esprit.gestionemployes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class DepartementHashSet implements IDepartement<Departement> {
+    private Set<Departement> departements;
+
+    public DepartementHashSet() {
+        this.departements = new HashSet<>();
+    }
+
+    @Override
+    public void ajouterDepartement(Departement departement) {
+        departements.add(departement);
+    }
+
+    @Override
+    public boolean rechercherDepartement(String nom) {
+        for (Departement departement : departements) {
+            if (departement.getNomDepartement().equals(nom)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean rechercherDepartement(Departement departement) {
+        return departements.contains(departement);
+    }
+
+    @Override
+    public void supprimerDepartement(Departement departement) {
+        departements.remove(departement);
+    }
+
+    @Override
+    public void displayDepartement() {
+        for (Departement departement : departements) {
+            System.out.println(departement);
+        }
+    }
+
+    @Override
+    public TreeSet<Departement> trierDepartementById() {
+        // This will require Departement to implement Comparable<Departement>
+        return new TreeSet<>(departements);
+    }
+}
